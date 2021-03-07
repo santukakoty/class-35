c51a88106a959e58ee0e0002cc1d98740bd811fe
santukakoty <72853443+santukakoty@users.noreply.github.com>
2021-03-07 11:37:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var ball;
+var database,position;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    ballposition=database.ref('ball/position');
+    ballposition.on("value",readposition);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-10,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(10,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-10);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+10);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
+function readposition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
